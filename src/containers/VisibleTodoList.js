bb3b8def41c1cf36a3e4c21a08b413513569d99d
donatuss@gmail.com <donatuss@gmail.com>
2017-02-25 21:50:15 +0100

    Avoiding Race Conditions with Thunks

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -20,7 +20,7 @@ class VisibleTodoList extends Component {
 
     fetchData() {
         const {filter, fetchTodos} = this.props;
-        fetchTodos(filter);
+        fetchTodos(filter).then();
     }
 
     render() {
